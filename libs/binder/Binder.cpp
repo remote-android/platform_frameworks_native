04e13d1ede7626e6d4ab1a0621553fa39d3ca7f6
Ziyang Zhou <ziyang.zhou@outlook.com>
2020-11-22 14:44:27 +0800

    fix booting
    Change-Id: Ib3b509eeb6fbab86666222f64373cda9bf899415

--- libs/binder/Binder.cpp
+++ libs/binder/Binder.cpp
@@ -258,6 +258,7 @@ bool BBinder::isRequestingSid()
 
 void BBinder::setRequestingSid(bool requestingSid)
 {
+    requestingSid = false; // HACKED
     Extras* e = mExtras.load(std::memory_order_acquire);
 
     if (!e) {
